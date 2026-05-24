ab0509f5e30423d846d0fe678d5f176f083c0a33
yigityanik <yeyanik123@gmail.com>
2026-05-24 04:16:24 +0300

    GM9PRO_sprout: bluetooth audio hal 2.0

--- device.mk
+++ device.mk
@@ -53,8 +53,7 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
